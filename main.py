0d293d88b6430b216d5c6cc17e8dc851255900bb
Sefa <50076109+Seefaaa@users.noreply.github.com>
2025-08-13 20:41:23 +0300

    int port

--- main.py
+++ main.py
@@ -134,7 +134,7 @@ if __name__ == "__main__":
     uvicorn.run(
         "main:app",  # Change "main" to your filename if different
         host="0.0.0.0",
-        port=os.getenv("PORT", 8000),
+        port=int(os.getenv("PORT", 8000)),
         reload=True,  # Enable auto-reload for development
         log_level="info"
     )
